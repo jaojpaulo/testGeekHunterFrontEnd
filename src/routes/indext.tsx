85b1a2901f5cd6c561f0c4605b9883bf38f1175e
jaojpaulo <jaojpaulosantos@gmail.com>
2021-06-26 12:02:54 -0300

    Commit inicial

---
+++ src/routes/indext.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Switch, Route } from 'react-router-dom';
+
+import Initial from '../pages/Initial';
+
+const Routes: React.FC = () => (
+    <Switch>
+        <Route path="/" exact component={Initial}/>
+    </Switch>
+);
+
+export default Routes;
