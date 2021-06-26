f93fad48984c231a57c484d972dfff2ce27cfb13
jaojpaulo <jaojpaulosantos@gmail.com>
2021-06-26 18:08:33 -0300

    Configurar estilos globais e adicionar fontes globais

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: 0;
+    }
+
+    body {
+        -webkit-font-smoothing: antialiased;
+        background: #21222c;
+    }
+
+    body, input, button{
+        font-family: 'Roboto Slab', serif;
+        font-size: 16px;
+    }
+
+    button{
+    cursor: pointer;
+  }
+`;
